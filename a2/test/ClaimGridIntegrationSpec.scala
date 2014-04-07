eac774d2e6849be86dc3996c3fb1a3350df83ed0
Mark Hunter <mark@huntersix.co.uk>
2014-04-07 13:57:25 +0100

    US617 - Added integration test for ClaimGrid

---
+++ a2/test/ClaimGridIntegrationSpec.scala
@@ -0,0 +1,20 @@
+import org.joda.time.format.DateTimeFormat
+import org.joda.time.LocalDate
+import org.specs2.mutable.{Specification, Tags}
+import play.api.test.WithBrowser
+import scala.collection.JavaConverters._
+
+class ClaimGridIntegrationSpec extends Specification with Tags {
+  "Claim Grid" should {
+    "Show claims filtered by today's date" in new WithBrowser {
+      browser.goTo("/")
+      browser.title() mustEqual("Claims list")
+      val today = DateTimeFormat.forPattern("dd/MM/yyyy").print(new LocalDate)
+      val receivedDates = browser.$("#claimsTable .receivedDate")
+
+      for (receivedDate <- receivedDates.asScala.toSeq) {
+        receivedDate.getText() mustEqual(today)
+      }
+    }
+  }
+}
