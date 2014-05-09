22b438a59359b90e35e03f21850902465b568a67
Chris Dennett <chdenno@gmail.com>
2014-05-09 15:03:07 +0100

    CASA login

--- HTMLTemplates/js/custom.js
+++ HTMLTemplates/js/custom.js
@@ -4,19 +4,16 @@ $(document).ready(function() {
 	$( ".theme-default" ).click(function() {
 	  $( "body" ).removeClass( "theme" );
 	  $( "body" ).removeClass( "red-theme" );
-	  return false;
 	});
     
 	$( ".theme-red" ).click(function() {
 	  $( "body" ).addClass( "red-theme" );
 	  $( "body" ).removeClass( "theme" );
-	  return false;
 	});
     
 	$( ".theme-blue" ).click(function() {
 	  $( "body" ).addClass( "theme" );
 	  $( "body" ).removeClass( "red-theme" );
-	  return false;
 	});
 
 
