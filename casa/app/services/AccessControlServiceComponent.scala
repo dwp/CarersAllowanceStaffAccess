8ea4dd3ef84adc4612b26cd694b3fc51139390ce
Ruben Diaz <ruben.diaz@valtech.co.uk>
2014-05-21 17:01:51 +0100

    Removing some logging.

--- casa/app/services/AccessControlServiceComponent.scala
+++ casa/app/services/AccessControlServiceComponent.scala
@@ -1,13 +1,10 @@
 package services
 
-import play.api.Logger
 
 trait AccessControlServiceComponent {
   def accessControlService = if (play.api.Play.current.configuration.getBoolean("enableStub") == Some(true)){
-    Logger.info("Using ac stub")
     AccessControlServiceStub
   }else{
-    Logger.info("Using ac impl")
     AccessControlServiceImpl
   }
 }
--- casa/app/services/ClaimServiceComponent.scala
+++ casa/app/services/ClaimServiceComponent.scala
@@ -1,14 +1,10 @@
 package services
 
-import play.api.Logger
-
 
 trait ClaimServiceComponent {
   def claimService = if (play.api.Play.current.configuration.getBoolean("enableStub") == Some(true)){
-    Logger.info("Using the mock")
     ClaimServiceMock
   } else {
-    Logger.info("Using the impl")
     ClaimServiceImpl
   }
 }
