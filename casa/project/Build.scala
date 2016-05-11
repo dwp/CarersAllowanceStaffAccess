54325044db6841186d3e2a5caa59650865dd5bf0
David Hutchinson <david@scrumconnect.com>
2016-05-11 10:51:45 +0100

    updated conf/application-info.conf

--- casa/project/Build.scala
+++ casa/project/Build.scala
@@ -6,7 +6,7 @@ import utils.ConfigurationChangeHelper._
 
 object ApplicationBuild extends Build {
   val appName = "sa"
-  val appVersion = "2.5-SNAPSHOT"
+  val appVersion = "2.6-SNAPSHOT"
 
   processConfFiles(Seq("conf/application-info.conf"), Seq("application.version" -> appVersion, "application.name" -> appName))
 
